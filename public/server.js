e0f450e3769740497b628f62cbfed7a7d9ed09a1
ProxyUnblocker <134707347+ProxyUnblocker@users.noreply.github.com>
2023-06-02 15:13:43 +0000

    Started files

---
+++ public/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Serve static files (including index.html)
+app.use(express.static('public'));
+
+// Proxy route
+app.get('/proxy', (req, res) => {
+  const url = req.query.url;
+
+  // Make a request to the provided URL
+  request(url, (error, response, body) => {
+    if (error) {
+      res.status(500).send('Error');
+    } else {
+      res.send(body);
+    }
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Proxy server is running on port ${port}`);
+});
