2be05ab7ae5e152dc1a5b5e5ab6101b76a638d6d
ProxyUnblocker <134707347+ProxyUnblocker@users.noreply.github.com>
2023-06-02 15:19:25 +0000

    Resetting Port

--- public/server.js
+++ public/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const request = require('request');
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 // Serve static files (including index.html)
 app.use(express.static('public'));
